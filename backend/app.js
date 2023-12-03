eb90732719ad3f95dcc53700d1e16fac3f5739be
Mohit kumar <127036501+mohitkumar54534@users.noreply.github.com>
2023-12-03 18:43:02 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -9,15 +9,18 @@ const app = express();
 // Middlewares
 app.use(express.json());
 app.use(cors());
-app.use("/users", router); // localhost:5000/books
+app.use("/users", router);
 
+const PORT = process.env.PORT || 6002;
 
 mongoose
   .connect(
-   "mongodb+srv://Mohit:8TvbN9BIkQ1rN4sC@cluster0.irv5cd4.mongodb.net/userstore?retryWrites=true&w=majority"
+    "mongodb+srv://Mohit:8TvbN9BIkQ1rN4sC@cluster0.irv5cd4.mongodb.net/userstore?retryWrites=true&w=majority"
   )
-  .then(() => console.log("Connected To Database"))
   .then(() => {
-    app.listen(6002);
+    console.log("Connected To Database");
+    app.listen(PORT, () => {
+      console.log(`App is listening at ${PORT}`);
+    });
   })
   .catch((err) => console.log(err));
